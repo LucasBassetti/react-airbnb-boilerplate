5257e3fc59c94742f8177587374bf352c7a9df4a
Lucas Bassetti <lucas@MacBook-Pro-de-Lucas.local>
2017-04-11 15:35:07 -0300

    fix component dependency

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import App from './component/App';
+import App from './components/App';
 
 render(
   <App />,
