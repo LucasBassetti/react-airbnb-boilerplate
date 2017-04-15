b9f4c0132ae56fc284b341578b4c921e610b2a20
Lucas Bassetti <lucas@MacBook-Pro-de-Lucas.local>
2017-04-15 18:51:07 -0300

    update configs

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -1,9 +1,8 @@
-import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 
 render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
